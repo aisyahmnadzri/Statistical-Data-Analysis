6ff3a01dbd8a5d207fec09c3f504ac17f89c1b5b
Aisyah Nadzri <aisyahmnadzri@gmail.com>
2025-09-18 20:20:53 +0800

    Update Chi-Square Test of Independence R Script.R

--- R Script File/Chi-Square Test/Chi-Square Test of Independence R Script.R
+++ R Script File/Chi-Square Test/Chi-Square Test of Independence R Script.R
@@ -1,23 +1,3 @@
-#-----------------------------------------------------
-# SEC12143: PROBABILITY STATISTICAL & DATA ANALYSIS   |
-#                                                     |
-# PROJECT 2                                           |   
-#-----------------------------------------------------
-
-#-------------------------------------------------------
-#                                                       |
-# Member Name: AHMAD MIRZA ARMAND BIN SHAZRIL FARIZA    |                                                                      
-# Matric Number: A21EC0006                              |
-#                                                       |
-# Analysis: Chi-Square Test of Independence             |
-#                                                       |
-# Objective of analysis: To observe whether there are   |
-#                        relationship exists between    |
-#                        two variables Gender and Degree|
-#                        Completion of Certificate at   |
-#                        significance level 0.05        |              
-#-------------------------------------------------------
-
 # Steps:
 #
 # 1) View our dataset
@@ -31,4 +11,5 @@ chisq.test (d,correct=FALSE)
 alpha <- 0.05
 x2.alpha <- qchisq(alpha, df=1, lower.tail= FALSE)
 print (x2.alpha)
+
 
